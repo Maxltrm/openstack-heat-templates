e76d758fa670bc9425f9a38a3dbf62398182bff4
Massimo Minniti <massimo.minniti@par-tec.it>
2018-12-24 16:52:04 +0100

    removed ansible parts

--- rhce_env/configure_ssh_login.sh
+++ rhce_env/configure_ssh_login.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-echo "[*] CONFIGURE SSH LOGIN AND ANSIBLE USER"
+echo "[*] CONFIGURE SSH LOGIN"
 echo "sshd: All" >> /etc/hosts.deny
 echo "sshd: $ALLOWED_CIDR" >> /etc/hosts.allow
 sed -i s/"PermitRootLogin no"/"PermitRootLogin yes"/g /etc/ssh/sshd_config
 systemctl restart sshd
 echo "$ROOT_PWD" | passwd --stding root
-useradd -g wheel -s /bin/bash ansible
-echo "$ANSIBLE_PWD" | passwd --stding ansible

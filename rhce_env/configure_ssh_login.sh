503b89c48352bbe39b0e691f075dede204f57366
Massimo Minniti <massimo.minniti@par-tec.it>
2018-12-24 10:28:46 +0100

    fix sshd path

--- rhce_env/configure_ssh_login.sh
+++ rhce_env/configure_ssh_login.sh
@@ -2,7 +2,7 @@
 echo "[*] CONFIGURE SSH LOGIN AND ANSIBLE USER"
 echo "sshd: All" >> /etc/hosts.deny
 echo "sshd: $ALLOWED_CIDR" >> /etc/hosts.allow
-sed -i s/"PermitRootLogin no"/"PermitRootLogin yes"/g sshd_config
+sed -i s/"PermitRootLogin no"/"PermitRootLogin yes"/g /etc/ssh/sshd_config
 systemctl restart sshd
 echo "$ROOT_PWD" | passwd --stding root
 useradd -g wheel -s /bin/bash ansible

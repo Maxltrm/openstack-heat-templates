eee499000ea213bc0f2eecba1ee333babe362d48
root <root@max.localdomain>
2018-12-24 10:15:41 +0100

    add ssh config script

---
+++ rhce_env/configure_ssh_login.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "[*] CONFIGURE SSH LOGIN AND ANSIBLE USER"
+echo "sshd: All" >> /etc/hosts.deny
+echo "sshd: $ALLOWED_CIDR" >> /etc/hosts.allow
+sed -i s/"PermitRootLogin no"/"PermitRootLogin yes"/g sshd_config
+systemctl restart sshd
+echo "$ROOT_PWD" | passwd --stding root
+useradd -g wheel -s /bin/bash ansible
+echo "$ANSIBLE_PWD" | passwd --stding ansible

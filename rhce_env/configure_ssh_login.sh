a43ff283cc724a11942d1a9849543e0cea109b81
Massimo Minniti <massimo.minniti@par-tec.it>
2018-12-26 12:49:18 +0100

    removed get_file

--- rhce_env/configure_ssh_login.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "[*] CONFIGURE SSH LOGIN"
-echo "sshd: All" >> /etc/hosts.deny
-echo "sshd: $ALLOWED_CIDR" >> /etc/hosts.allow
-sed -i s/"PermitRootLogin no"/"PermitRootLogin yes"/g /etc/ssh/sshd_config
-systemctl restart sshd
-echo "$ROOT_PWD" | passwd --stding root
